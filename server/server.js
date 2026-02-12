93c7c44718f59af371bf18d51e6e676b9a8bc947
Tahirali <aliparbadiay786@gmail.com>
2026-02-12 13:15:03 +0530

    allowing localhost for offline

--- server/server.js
+++ server/server.js
@@ -16,8 +16,22 @@ connectDB();
 const app = express();
 
 // Middleware
+const allowedOrigins = [
+    "https://luxeecom.vercel.app",
+    "http://localhost:5173",
+    "http://localhost:8080" // Adding 3000 just in case
+];
+
 app.use(cors({
-    origin: "https://luxeecom.vercel.app",
+    origin: function (origin, callback) {
+        // allow requests with no origin (like mobile apps or curl requests)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
