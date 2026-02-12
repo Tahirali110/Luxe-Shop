7e108a5ac93c8444ab83e02e76eea1b627741bf6
Tahirali <aliparbadiay786@gmail.com>
2026-02-12 12:11:25 +0530

    remove / from link

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: "https://luxeecom.vercel.app/",
+    origin: "https://luxeecom.vercel.app",
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
