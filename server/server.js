894d5aa75fced223a9e7deee77592e687d9e8bef
Tahirali <aliparbadiay786@gmail.com>
2026-02-12 12:10:09 +0530

    server & client deploy on vercel

--- server/server.js
+++ server/server.js
@@ -16,7 +16,12 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: "https://luxeecom.vercel.app/",
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // Default root route
